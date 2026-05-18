ed297d5938cbbef9c36c1ae4203968144fc70114
keengarthur <arthurchimaroke@gmail.com>
2026-05-18 21:27:54 +0100

    feat: Update API server URL in Swagger configuration

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
 
     servers: [
       {
-        url: "http://localhost:5000/api/v1",
+        url: "https://three60degreeshr-iewp.onrender.com/api/v1",
         description: "Development server",
       },
     ],
