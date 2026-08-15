f29f7220183dae4fc42cbfd473782db66d162b05
keengarthur <arthurchimaroke@gmail.com>
2026-08-15 16:31:15 +0100

    Add authentication middleware to performance routes for enhanced security

--- src/modules/performance/performance.routes.ts
+++ src/modules/performance/performance.routes.ts
@@ -2,19 +2,20 @@ import { Router } from "express";
 import { validate } from "../../shared/middleware/validate.middleware.js";
 import { PerformanceController } from "./performance.controller.js";
 import { performanceReviewSchema, performanceTemplateSchema, updatePerformanceTemplateSchema } from "./performance.validation.js";
+import { parseAuthHeaderMiddleware } from "../../shared/middleware/auth.middleware.js";
 
 
 const router = Router()
 
-router.post("/template" , validate(performanceTemplateSchema), PerformanceController.createPerformanceReviewController)
-router.patch("/template/templateId" , validate(updatePerformanceTemplateSchema), PerformanceController.updatePerformanceTemplateController)
-router.get("/template" , PerformanceController.getAllPerformanceTemplateController)
-router.get("/template/:templateId" , PerformanceController.getSinglePerformanceTemplateController)
-router.delete("/template/:templateId" , PerformanceController.deleteSinglePerformanceTemplateController)
-router.post("/review" ,  validate(performanceReviewSchema) ,PerformanceController.createPerformanceReviewController)
-router.get("/review" ,  PerformanceController.getMyReviewsController)
-router.get("/review/:reviewId" , PerformanceController.getMyReviewTasksController)
-router.get("/review/instance" , PerformanceController.getAllReviewInstancesController)
-router.patch("/review/:reviewId" , PerformanceController.closePerformanceReviewController)
+router.post("/template" , parseAuthHeaderMiddleware() ,validate(performanceTemplateSchema), PerformanceController.createPerformanceReviewController)
+router.patch("/template/templateId" , parseAuthHeaderMiddleware(), validate(updatePerformanceTemplateSchema), PerformanceController.updatePerformanceTemplateController)
+router.get("/template" , parseAuthHeaderMiddleware(), PerformanceController.getAllPerformanceTemplateController)
+router.get("/template/:templateId" , parseAuthHeaderMiddleware(), PerformanceController.getSinglePerformanceTemplateController)
+router.delete("/template/:templateId" ,parseAuthHeaderMiddleware(),  PerformanceController.deleteSinglePerformanceTemplateController)
+router.post("/review" ,  parseAuthHeaderMiddleware(), validate(performanceReviewSchema) ,PerformanceController.createPerformanceReviewController)
+router.get("/review" , parseAuthHeaderMiddleware(),  PerformanceController.getMyReviewsController)
+router.get("/review/:reviewId" ,parseAuthHeaderMiddleware(),  PerformanceController.getMyReviewTasksController)
+router.get("/review/instance" ,parseAuthHeaderMiddleware(),  PerformanceController.getAllReviewInstancesController)
+router.patch("/review/:reviewId" ,parseAuthHeaderMiddleware(),  PerformanceController.closePerformanceReviewController)
 
 export default router
