8276b0b8811a9d81dd92d7c526a6fd192f28c3ba
keengarthur <arthurchimaroke@gmail.com>
2026-08-29 18:48:52 +0100

    Fix : Missing HOD check

--- src/modules/employee/employee.routes.ts
+++ src/modules/employee/employee.routes.ts
@@ -12,15 +12,15 @@ const upload = multer({
     storage : multer.memoryStorage()
 })
 
-router.post("/" , parseAuthHeaderMiddleware() ,parseAuthorizationMiddleware(), requiredPermission(["create_employe"]), validate(createEmployeeSchema ) , EmployeeController.createEmployeeController)
+router.post("/" , parseAuthHeaderMiddleware() ,parseAuthorizationMiddleware(), requiredPermission(["create_employee"]), validate(createEmployeeSchema ) , EmployeeController.createEmployeeController)
 router.get("/bulk-upload/template", parseAuthHeaderMiddleware(), EmployeeController.downloadBulkUploadTemplateController,
 );
 router.post("/bulk-uploads" , parseAuthHeaderMiddleware(), upload.single("file"), EmployeeController.createBulkEmployeeViaSheetController)
 router.get("/" , parseAuthHeaderMiddleware(), EmployeeController.getAllEmployeesController)
 router.get("/for-employee" , parseAuthHeaderMiddleware(), EmployeeController.getAllForEmployeeController)
 router.get("/:employeeId" , parseAuthHeaderMiddleware(), EmployeeController.getSingleEmployeeController)
-router.put("/:employeeId" , parseAuthHeaderMiddleware(), requiredPermission(["update_employee"]), validate(updateEmployeeSchema) ,EmployeeController.updateEmployeeController)
-router.delete("/:employeeId" , parseAuthHeaderMiddleware(), requiredPermission(["delete_employee"]) , EmployeeController.deleteEmployeeController)
+router.put("/:employeeId" , parseAuthHeaderMiddleware(), parseAuthorizationMiddleware(),requiredPermission(["update_employee"]), validate(updateEmployeeSchema) ,EmployeeController.updateEmployeeController)
+router.delete("/:employeeId" , parseAuthHeaderMiddleware(),parseAuthorizationMiddleware(), requiredPermission(["delete_employee"]) , EmployeeController.deleteEmployeeController)
 
 
 export default router
